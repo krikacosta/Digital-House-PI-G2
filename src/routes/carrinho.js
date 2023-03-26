fd91c17bc8a79a6e7cb95fd84ed377e73e336e01
Krikacosta <cris.artesanatosctba@gmail.com>
2023-03-25 21:11:48 -0300

    feat: Implementado Dinamismo Página Home e Produto (conexão com o DB), iniciado a implementação de dinamismo do carrinho, corrigido arquivos componentes (header e footer)

--- src/routes/carrinho.js
+++ src/routes/carrinho.js
@@ -4,6 +4,7 @@ const CarrinhoController = require('../controllers/CarrinhoController');
 const router = express.Router();
 
 router.get('/carrinho', CarrinhoController.showCarrinhoPage);
+router.get('/carrinho/:id', CarrinhoController.showCarrinhoPage);
 
 
 module.exports = router;
--- src/routes/home.js
+++ src/routes/home.js
@@ -4,6 +4,6 @@ const HomeController = require('../controllers/HomeController');
 const router = express.Router();
 
 router.get('/', HomeController.showHomePage);
-
+// router.get('/produtos/:id', HomeController.showProduto);
 
 module.exports = router;
